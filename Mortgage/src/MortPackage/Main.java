c78435d43dd7f0c7b64cd2d2c97b017eea3bac38
mintl <mintl@The_Slab>
2023-08-23 15:39:06 +0100

    first commit

---
+++ Mortgage/src/MortPackage/Main.java
@@ -0,0 +1,17 @@
+package MortPackage;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numbers = {2,5,6,8,4};
+		Arrays.sort(numbers);
+		System.out.println(Arrays.toString(numbers));
+		
+		
+	}
+
+}
