e54efa0413ec8256d1c4ac2925c45186c7ca4959
mintl <mintl@The_Slab>
2023-08-23 18:17:27 +0100

    Tidy code

--- Mortgage/src/MortPackage/Main.java
+++ Mortgage/src/MortPackage/Main.java
@@ -9,23 +9,20 @@ public class Main {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Principal: ");
+		System.out.print("Principal: ");	//Prompt for principal.
 		int principal = scanner.nextInt();
 		
 		System.out.print("Interest Rate: ");	//Prompt for interest rate.
 		float annualRate = scanner.nextFloat();
 		float monthlyRate = annualRate / 100 / 12;	//Convert the annual interest rate to monthly.
 		
-		System.out.print("Term (years): ");
+		System.out.print("Term (years): ");	//Prompt for term
 		int term = scanner.nextInt();
 		int numberOfPayments = term * 12; //Convert the term in years to months.
-		
-				
 		
 		double payments = principal * (monthlyRate * Math.pow(1 + monthlyRate, numberOfPayments))
 									/ (Math.pow(1 + monthlyRate, numberOfPayments) - 1);
 
-			//	) (principal * (rate( 1 + rate)*term) / (1 + rate) - 1) ;
 		String paymentsFormatted = NumberFormat.getCurrencyInstance().format(payments);	//Format result to output as local currency. 
 		System.out.println("Monthly Paymments: " + paymentsFormatted);
 		
